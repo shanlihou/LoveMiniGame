d09a36cf8eee7f25087fd3ff376fbc13e0d125d6
shanlihou <shanlihou@gmail.com>
2025-05-20 10:23:08 +0800

    up

---
+++ assets/game/scripts/common/constant.ts
@@ -0,0 +1,21 @@
+const HIT_TYPE_PINCH = 1;
+const HIT_TYPE_POKE = 2;
+const HIT_TYPE_POKE_DOUBLE = 3;
+const HIT_TYPE_FIST = 4;
+const HIT_TYPE_SLAP = 5;
+
+const HIT_POS_LEFT_FACE = 1;
+const HIT_POS_RIGHT_FACE = 2;
+const HIT_POS_NOSE = 3;
+const HIT_POS_MOUTH = 4;
+const HIT_POS_FOREHEAD = 5;
+const HIT_POS_CHIN = 6;
+const HIT_POS_BODY = 7;
+const HIT_POS_HAIR = 8;
+const HIT_POS_LEFT_EYE = 9;
+const HIT_POS_RIGHT_EYE = 10;
+
+
+
+
+
